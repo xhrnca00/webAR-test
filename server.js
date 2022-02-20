153cb81d9cb5ed5acd7b3e7b79fe68b13dca7417
xhrnca00 <89995247+xhrnca00@users.noreply.github.com>
2022-02-20 21:48:04 +0100

    removed https certificates from server

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const https = require("https");
 const fs = require("fs");
 
 const options = {
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("cert.pem"),
+    // key: fs.readFileSync("key.pem"),
+    // cert: fs.readFileSync("cert.pem"),
 };
 const file = new static.Server();
 
